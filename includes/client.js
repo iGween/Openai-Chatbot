38d6f23a7b60e7e85d447805df3dc8b7ebb7cc27
/Void <82276303+CrewMate019@users.noreply.github.com>
2024-01-05 14:55:51 +0700

    Create client.js

---
+++ includes/client.js
@@ -0,0 +1,62 @@
+const chalk = require('chalk');
+const config = require('../config/settings.js');
+
+module.exports = async ({
+  client,
+  messages
+}) => {
+  try {
+    const body = messages.mtype === 'conversation' ? messages.message.conversation : messages.mtype === 'extendedTextMessage' ? messages.message.extendedTextMessage.text : '';
+    const budy = typeof messages.text === 'string' ? messages.text : '';
+    const command = body.startsWith(config.prefix) ? body.replace(config.prefix, '').trim().split(/ +/).shift().toLowerCase() : '';
+    const cleanCommand = command.replace(config.prefix, '');
+    const args = body.trim().split(/ +/).slice(1);
+    const query = q = args.join(' ');
+    const query1 = q1 = query.split('|')[0]
+    const query2 = q2 = query.split('|')[1]
+
+    const ownerNumbers = config.owner.number;
+    const senderNumber = messages.sender.replace(/\D/g, '');
+    const senderName = messages.pushName || 'Undefined';
+    const isOwner = ownerNumbers.includes(messages.sender);
+
+    if (!config.public_mode) {
+      if (!messages.key.fromMe) {
+        return;
+      };
+    };
+
+    if (messages.message) {
+      client.readMessages([messages.key]);
+
+      console.log(
+        chalk.bgMagenta(' [New Message] '),
+        chalk.cyanBright('Time: ') + chalk.greenBright(new Date()) + '\n',
+        chalk.cyanBright('Message: ') + chalk.greenBright(budy || messages.mtype) + '\n' +
+        chalk.cyanBright('From:'), chalk.greenBright(senderName), chalk.yellow('- ' + senderNumber) + '\n' +
+        chalk.cyanBright('Chat Type:'), chalk.greenBright(!messages.isGroup ? 'Private Chat' : 'Group Chat - ' + chalk.yellow(messages.chat))
+      );
+    };
+    
+    try {            
+      const query = body;      
+      const fetchData = async (question) => {
+        const response = await fetch(`https://api.neoxr.eu/api/gpt?q=${question}`);
+        
+        return response.json();
+      };
+      
+      const data = await fetchData(query);
+      console.log(data);
+      
+      const response = data.data.message;
+      messages.reply(response);
+    } catch (error) {
+      console.error(error.message);
+    };
+    
+  } catch (error) {
+    messages.reply('Terjadi kesalahan pada server.');
+    console.error(error);
+  };
+};
