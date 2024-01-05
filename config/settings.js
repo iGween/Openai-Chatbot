218ead5f91014751990ea8f5456c2d868b1c2a9e
/Void <82276303+CrewMate019@users.noreply.github.com>
2024-01-05 14:53:42 +0700

    Add files via upload

---
+++ config/settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pairing_mode: true,
+  session_folder_name: 'session',
+  browser: ["Chrome (Linux)"],
+  public_mode: true, //False solo, True Public
+  bot: {
+    name: 'OpenAi オープンナイ'
+  },
+  owner: {
+    name: 'Ragnvindr',
+    number: '6281224994511'
+  },
+};
